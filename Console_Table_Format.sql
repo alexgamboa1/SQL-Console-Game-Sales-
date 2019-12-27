2aa40127567492457159e1f65ae3360d6760e42a
Alex G <54568578+alexgamboa1@users.noreply.github.com>
2019-12-26 21:36:32 -0800

    Update Console_Table_Format.sql

--- Console_Table_Format.sql
+++ Console_Table_Format.sql
@@ -1,14 +1,14 @@
 CREATE TABLE console_games (
 	game_rank integer, 
 	game_name  varchar(1200), 
-	 platform varchar(1200),
-    game_year integer,
-    genre varchar(20),
-    publisher varchar(1200),
-    na_sales float8,
-    eu_sales float8,
-    jp_sales float8,
-    other_sales float8    
+	platform varchar(1200),
+    	game_year integer,
+    	genre varchar(20),
+    	publisher varchar(1200),
+   	na_sales float8,
+    	eu_sales float8,
+    	jp_sales float8,
+    	other_sales float8    
 );
 
 CREATE TABLE console_dates (
